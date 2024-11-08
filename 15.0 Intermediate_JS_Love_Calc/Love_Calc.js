444dae8948b5397011cf446db4e14ffaa9dbc570
WithTempo <connor.ryan.mcgaughey@proton.me>
2024-11-08 16:12:07 -0600

    complete dice game challenge

--- 15.0 Intermediate_JS_Love_Calc/Love_Calc.js
+++ 15.0 Intermediate_JS_Love_Calc/Love_Calc.js
@@ -5,6 +5,6 @@ function Love_Calculator(name_1, name_2){
 
 console.log("Welcome to Love Calculator!");
 console.log("Enter both your name and someone else's name to see your compatibility rating out of 100")
-var name_1 = prompt("What is your name?");
-var name_2 =prompt("What is the other person's name?");
+var name_1 = console.prompt("What is your name?");
+var name_2 = console.prompt("What is the other person's name?");
 Love_Calculator(name_1, name_2)
